c4b68c7f47a6247d91fecccee37a54a40f0dc5bf
hikaruonitsuka <linlin.h071109@gmail.com>
2023-06-18 11:55:09 +0900

    アナリティクスの導入

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,13 @@ import '@/styles/globals.scss';
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'next-themes';
 import { DefaultSeo } from 'next-seo';
+import { Analytics } from '@vercel/analytics/react';
 import SEO from '@/lib/seoConfig';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <ThemeProvider attribute='class' enableSystem={true}>
+      <Analytics />
       <DefaultSeo {...SEO} />
       <Component {...pageProps} />
     </ThemeProvider>
