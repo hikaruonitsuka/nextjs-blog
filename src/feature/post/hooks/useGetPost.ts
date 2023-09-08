261bc2c1a6b91829cea6aa57be41e7af540b65c5
hikaruonitsuka <linlin.h071109@gmail.com>
2023-09-08 15:43:28 +0900

    [Add #4] シンタックスハイライト機能の追加

--- src/feature/post/hooks/useGetPost.ts
+++ src/feature/post/hooks/useGetPost.ts
@@ -1,4 +1,6 @@
 import { client } from '@/utils/client';
+import hljs, { HighlightResult } from 'highlight.js';
+import { load } from 'cheerio';
 
 // ディレクトリパス
 const PATH = '/post/';
@@ -13,6 +15,35 @@ export const getStaticPaths = async () => {
 export const getStaticProps = async (context: any) => {
   const id = context.params.id;
   const data = await client.get({ endpoint: 'blog', contentId: id });
+
+  /**
+   * シンタックスハイライトの追加
+   * @see https://codeseterpie.com/blog/e91oc4aaef58/
+   */
+
+  // APIから取得したリッチエディタのHTMLからcheerioオブジェクトを生成
+  const $ = load(data.body);
+
+  // コードブロックのシンタックスハイライトを行う
+  $('pre code').each((_, elm) => {
+    const language = $(elm).attr('class') || '';
+    let result: HighlightResult;
+
+		result = hljs.highlightAuto($(elm).text());
+    // if (language == '') {
+    //   // 言語が入力なしの場合、自動判定
+    // } else {
+    //   // 言語が入力ありの場合、入力された言語で判定
+    //   result = hljs.highlight($(elm).text(), {
+    //     language: language.replace('language-', ''),
+    //   });
+    // }
+    $(elm).html(result.value);
+    $(elm).addClass('hljs');
+  });
+
+  // 編集したHTMLを再設定
+  data.body = $.html();
 
   // 前後記事の取得
   const prev = await client.get({
