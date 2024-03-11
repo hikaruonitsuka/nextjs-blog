3089cc7b64f4112a683dc349d13467dab8f07077
hikaruonitsuka <linlin.work071109@gmail.com>
2024-03-11 20:43:42 +0900

    [add]:microcmsクライアントを追加

---
+++ src/libs/microcms.ts
@@ -0,0 +1,43 @@
+import { Blog } from '@/types/Blog';
+import { createClient } from 'microcms-js-sdk';
+import type { MicroCMSQueries } from 'microcms-js-sdk';
+
+if (!process.env.SERVICE_DOMAIN) {
+  throw new Error('SERVICE_DOMAIN is required');
+}
+
+if (!process.env.API_KEY) {
+  throw new Error('API_KEY is required');
+}
+
+export const client = createClient({
+  serviceDomain: process.env.SERVICE_DOMAIN,
+  apiKey: process.env.API_KEY,
+});
+
+// 投稿一覧を取得
+export const getList = async (queries?: MicroCMSQueries) => {
+  const listData = await client.getList<Blog>({
+    endpoint: 'blog',
+    queries,
+  });
+
+  // データの取得が目視しやすいよう明示的に遅延効果を追加
+  await new Promise((resolve) => setTimeout(resolve, 3000));
+
+  return listData;
+};
+
+// 投稿詳細ページを取得
+export const getDetail = async (contentId: string, queries?: MicroCMSQueries) => {
+  const detailData = await client.getListDetail<Blog>({
+    endpoint: 'blog',
+    contentId,
+    queries,
+  });
+
+  // データの取得が目視しやすいよう明示的に遅延効果を追加
+  await new Promise((resolve) => setTimeout(resolve, 3000));
+
+  return detailData;
+};
